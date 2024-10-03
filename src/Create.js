691bd877abf9d62c9c74f8e1ae7d1451b9150b43
cheikh ibrahima dieng <barhamadieng66@gmail.com>
2024-10-03 20:21:14 +0000

    Ajout d'un blog

--- src/Create.js
+++ src/Create.js
@@ -1,16 +1,44 @@
+import {useState} from "react";
+import useFetch from "./useFetch";
+
 const Create = () => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('');
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // let data = new FormData(e.target)
+        // console.log(data.get('title'));
+        const blog = { title, body, author};
+        // const {data: blog1, isPending, error} = useFetch("http://localhost:8084/blogs");
+        fetch("http://localhost:8084/blogs",{
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(blog)
+        }).then(data => {
+            if (!data.ok) {
+                throw new Error(`Mon HTTP error! status: ${data.status}`);
+            }
+            console.log(data);
+            setTitle('');
+            setBody('');
+            setAuthor('');
+        })
+    }
     return (
         <div className="create">
             <h2>Add a new blog</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Blog title:</label>
-                <input type="text" required/>
+                <input type="text" name="title" value={title} onChange={(e) => setTitle(e.target.value)} required/>
                 <label>Blog body:</label>
-                <textarea required>
+                <textarea required name="body" value={body} onChange={(e) => setBody(e.target.value)}>
 
                 </textarea>
                 <label>Blog author:</label>
-                <select>
+                <select name="author" value={author} onChange={(e) => setAuthor(e.target.value)}>
                     <option value="mario">mario</option>
                     <option value="yoshi">yoshi</option>
                 </select>
